1fb74e5e1a5ab882f5782376dde5bb0fb893b4c6
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-15 08:12:17 +0500

    Task1

---
+++ Tyuiu.MalcevDV.Sprint2.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MalcevDV.Sprint2.Task1.V18.Lib
+{
+    public class DataService : ISprint2Task1V18
+    {
+
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] result = new bool[6];
+            result[0] = !(a > d);
+            result[1] = (a < b) & (c != d);
+            result[2] = (a < b) || (c > d);
+            result[3] = (a > d) && (c < d);
+            result[4] = !(a == d);
+            result[5] = (a < b) ^ (d >= c);
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint2.Task1.V18/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.MalcevDV.Sprint2.Task1.V18.Lib;
+DataService ds = new DataService();
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #2", width);
+PrintCenteredLine("Тема: Создание решения по таску", width);
+PrintCenteredLine("Задание #1", width);
+PrintCenteredLine("Вариант #18", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, вычисляющую значения логических операций", width);
+PrintCenteredLine("используя четыре переменные", width);
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ: a = 155, b = 696, c = 242, d = 541", width);
+int a = 155;
+int b = 696;
+int c = 242;
+int d = 541;
+bool[] res = new bool[6];
+res = ds.GetLogicOperations(a, b ,c, d);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+for (int i = 0; i < 6; i++)
+{
+    PrintCenteredLine($"{res[i]}", width);
+}
+Console.WriteLine(new string('*', width));
