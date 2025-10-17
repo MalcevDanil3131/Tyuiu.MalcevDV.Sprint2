3e4b8e25f1714b7d3d57afdb9450ada50d055acc
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-18 02:04:34 +0500

    Task5

--- Tyuiu.MalcevDV.Sprint2.Task5.V14.Test/DataServiceTest.cs
+++ Tyuiu.MalcevDV.Sprint2.Task5.V14.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.MalcevDV.Sprint2.Task5.V14.Test
             DataService ds = new DataService();
             int d = 1;
             int k = 5;
-            var wait = "пятница";
+            var wait = "Пятница";
             var res = ds.FindDayName(d, k);
             Assert.AreEqual(wait, res);
         }
